fe3b86b2c8ca664202c808c25fbe760fc9d3d111
Mohammad Kazemi <kazemi.me.222@gmail.com>
2021-10-19 18:12:05 +0330

    Fixed poet_test.py

--- tests/poet_test.py
+++ tests/poet_test.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MIT
 
 from pytest import fixture
-from ganjoor_wrapper import Poet
+from ganjoor import Poet
 import vcr
 
 
@@ -10,27 +10,25 @@ class TestPoet:
 
     @fixture
     def poet_keys(self):
-        return ['id', 'name', 'description', 'fullUrl', 'rootCatId',
-                'nickname', 'published', 'imageUrl']
+        return ['_id', '_name', '_description', '_full_url', '_root_cat_id',
+                '_nickname', '_published', '_image_url']
 
     @vcr.use_cassette('tests/vcr_cassettes/poet_details.yml')
     def test_poet_details(self, poet_keys):
         """Tests an API call to get a Poet's details"""
-        poet_instance = Poet(57)
-        response = poet_instance.details()
-        assert isinstance(response, dict)
-        assert response['id'] == 57, "The ID should be in the response"
-        assert set(poet_keys).issubset(response.keys()
+        poet_instance = Poet.find(57)
+        assert isinstance(poet_instance, Poet)
+        assert poet_instance.id == 57, "The ID should be in the poet_instance"
+        assert set(poet_keys).issubset(poet_instance.__dict__.keys()
                                        ), "All keys should be in the response"
 
     @vcr.use_cassette('tests/vcr_cassettes/poet_details_url.yml')
     def test_poet_details_url(self, poet_keys):
         """Tests an API call to get a Poet's details using its url"""
-        poet_instance = Poet(url="/hafez")
-        response = poet_instance.details()
-        assert isinstance(response, dict)
-        assert response['fullUrl'] == '/hafez', "The ID should be in the response"
-        assert set(poet_keys).issubset(response.keys()
+        poet_instance = Poet.find_by_url(url="/hafez")
+        assert isinstance(poet_instance, Poet)
+        assert poet_instance.full_url == '/hafez', "The url should be in the response"
+        assert set(poet_keys).issubset(poet_instance.__dict__.keys()
                                        ), "All keys should be in the response"
 
     @vcr.use_cassette('tests/vcr_cassettes/poets_all.yml')
@@ -39,4 +37,4 @@ class TestPoet:
         assert isinstance(poets, list)
         for poet in poets:
             assert set(poet_keys).issubset(
-                poet.keys()), "All keys should be in response"
+                poet.__dict__.keys()), "All keys should be in poet_instance"
