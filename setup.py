dae1983cdcd18c5ded3a2bad05f6bf79c70e4a27
Mohammad Kazemi <kazemi.me.222@gmail.com>
2021-10-25 16:17:40 +0330

    Changed Version to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='ganjoor-api-wrapper',
     packages=['ganjoor'],
-    version='0.1',
+    version='0.1.1',
     license='MIT',
     description='Ganjoor API wrapper in python',
     author='Mohammad Kazemi',
