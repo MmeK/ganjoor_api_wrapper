2d190ef9ed471d38d98d3f6a2efeb87eb9737310
Mohammad Kazemi <kazemi.me.222@gmail.com>
2021-11-02 16:43:16 +0330

    Added very rudimentary caching with requests-cache monkey-patching

--- ganjoor/__init__.py
+++ ganjoor/__init__.py
@@ -2,4 +2,17 @@
 # SPDX-License-Identifier: MIT
 from .models import (Poet, Poem, Category)
 from .ganjoor import Ganjoor
+from .config import GANJGAH_BASE_URL
 from .exceptions import GanjoorException
+import requests_cache
+from requests_cache import DO_NOT_CACHE
+
+urls_expire_after = {
+    GANJGAH_BASE_URL+Poem._urls['random']: DO_NOT_CACHE,
+    GANJGAH_BASE_URL+Poem._urls['hafez_faal']: DO_NOT_CACHE,
+    '*': -1
+}
+
+
+requests_cache.install_cache(
+    cache_name='ganjoor_cache', backend='sqlite', urls_expire_after=urls_expire_after)
